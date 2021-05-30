476dbdefef201186d83c9c3a3aa92378ba9ab997
htalebiyan <35233350+htalebiyan@users.noreply.github.com>
2021-05-30 17:18:21 -0500

    Run sensitivity for synthetic

--- codes/Sensitivity_SALib/plot.py
+++ codes/Sensitivity_SALib/plot.py
@@ -10,111 +10,122 @@ sns.set(style='darkgrid', font_scale=1)
 # plt.rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
 plt.close('all')
 
-def plot_radar(raw_data,row_titles,col_titles, suffix):
+
+def plot_radar(raw_data, row_titles, col_titles, suffix):
     # initialize the figure
-    my_dpi=300
-    plt.figure(figsize=(800/my_dpi, 800/my_dpi), dpi=my_dpi)
+    my_dpi = 300
+    plt.figure(figsize=(800 / my_dpi, 800 / my_dpi), dpi=my_dpi)
     # Initialise the spider plot
-    ax = plt.subplot(1,1,1, polar=True)
+    ax = plt.subplot(1, 1, 1, polar=True)
     # Create a color palette:
-    my_palette = ['darkorange','forestgreen','crimson','midnightblue','c','k']
-    #correct labels
+    my_palette = ['darkorange', 'forestgreen', 'crimson', 'midnightblue', 'c', 'k']
+    # correct labels
     labels = [x for x in row_titles]
     labels = [r'$R_c$' if x == 'Rc' else x for x in labels]
     labels = [r'$P_d$' if x == 'pd' else x for x in labels]
     labels = [r'$P_i$' if x == 'pi' else x for x in labels]
-    labels = [r'$\Upsilon$' if x == 'gamma' else x for x in labels]
+    # labels = [r'$\Upsilon$' if x == 'gamma' else x for x in labels]
     means = raw_data.mean(axis=1)
     # Loop to plot
     for row in range(len(raw_data.index)):
-        title=row_titles[row]
-        labels[row] = labels[row]+'(%1.2f)'%means[title]
-        color=my_palette[row]
+        title = row_titles[row]
+        labels[row] = labels[row] + '(%1.2f)' % means[title]
+        color = my_palette[row]
         # number of variable
-        categories=list(raw_data)
+        categories = list(raw_data)
         N = len(categories)
-         
+
         # What will be the angle of each axis in the plot? (we divide the plot / number of variable)
         angles = [n / float(N) * 2 * pi for n in range(N)]
         angles += angles[:1]
-         
+
         # If you want the first axis to be on top:
         ax.set_theta_offset(pi / 2)
         ax.set_theta_direction(-1)
-         
+
         # Draw one axe per variable + add labels labels yet
-        plt.xticks(angles[:-1], col_titles, color='k', y=-.03)
+        plt.xticks(angles[:-1], col_titles, color='k', y=-.2)
         # Draw ylabels
         ax.set_rlabel_position(10)
-        plt.yticks([1,2,3,4,5,6], ["1","2","3","4","5","6"], color="k")
-        plt.ylim(1,7)
+        plt.yticks([1, 2, 3, 4], ["1", "2", "3", "4"], color="k")
+        plt.ylim(1, 5)
         ax.invert_yaxis()
-         
-        values=raw_data.iloc[row].values.flatten().tolist()
+
+        values = raw_data.iloc[row].values.flatten().tolist()
         values += values[:1]
         ax.plot(angles, values, color=color, linewidth=0.4, linestyle='solid', zorder=6)
-        ax.fill(angles, values, color=color, alpha=0.2, lw=0.01,zorder=6)
+        ax.fill(angles, values, color=color, alpha=0.2, lw=0.01, zorder=6)
     # Add legend and save to file
-    leg = plt.figlegend(ax.lines,labels,loc='lower center', bbox_to_anchor=(0.6, 1.05),
-                        ncol=3, fontsize='medium',frameon=False)
+    leg = plt.figlegend(ax.lines, labels, loc='lower center', bbox_to_anchor=(0.5, 1.05),
+                        ncol=2, fontsize='medium', frameon=False)
     for legobj in leg.legendHandles:
         legobj.set_linewidth(1.5)
-    plt.savefig('SensRadar_'+suffix+'.png', dpi=my_dpi, bbox_inches="tight",bbox_extra_artists=(leg, ))
-    
+    plt.savefig('SensRadar_' + suffix + '.png', dpi=my_dpi, bbox_inches="tight", bbox_extra_artists=(leg,))
+
+
+def correct_legend_labels(labels):
+    labels = ['N-INRG' if x == 'ng' else x for x in labels]
+    labels = ['B-INRG-All cu' if x == 'bgCCCCUUUU' else x for x in labels]
+    labels = ['B-INRG-All cu ex. Power' if x == 'bgCCNCUUUU' else x for x in labels]
+    labels = ['B-INRG-All nu' if x == 'bgNNNNUUUU' else x for x in labels]
+    labels = ['B-INRG-nn' if x == 'bgNNUU' else x for x in labels]
+    labels = ['B-INRG-cn' if x == 'bgCNUU' else x for x in labels]
+    labels = ['B-INRG-nc' if x == 'bgNCUU' else x for x in labels]
+    labels = ['B-INRG-cc' if x == 'bgCCUU' else x for x in labels]
+    labels = ['Rationality' if x == 'rationality' else x for x in labels]
+    labels = ['br_level' if x == 'bounded rationality level' else x for x in labels]
+    return labels
+
+
 '''Plot results'''
-[corr, sens_perf, sens_res, delta_dict_perf, delta_dict_res,anova_perf, anova_res] = pd.read_pickle('postprocess_dicts_sens_synth.pkl')
+[corr, sens_perf, delta_dict_perf, anova_perf] = pd.read_pickle('postprocess_dicts_sens_synth_EDM10.pkl')
+
+for idx, r in sens_perf.iterrows():
+    sens_perf.loc[idx, "Decision/Res. Alloc."] = correct_legend_labels([r["decision"]])[0] + '\n' + r["auction"]
+for idx, r in corr.iterrows():
+    corr.loc[idx, "Decision/Res. Alloc."] = correct_legend_labels([r["decision_type"]])[0] + '-' + r["auction_type"]
 
 '''Radar plot'''
-sens_perf["Topology/Resource_allocation"] = sens_perf["auction"]+'\n'+sens_perf["topology"]
-# sens_perf = sens_perf.pivot_table(values='rank_corrected', index='config_param', columns='Topology/Resource_allocation')
-# sens_perf.reset_index()
-# plot_radar(sens_perf,sens_perf.index.values,sens_perf.columns, suffix='performance')
-# print(sens_perf.mean(axis=1))
+sens_perf_pivot = sens_perf.pivot_table(values='rank_corrected', index='config_param', columns='Decision/Res. Alloc.')
+sens_perf_pivot.reset_index()
+plot_radar(sens_perf_pivot, sens_perf_pivot.index.values, sens_perf_pivot.columns, suffix='performance')
+print(sens_perf_pivot.mean(axis=1))
 
-sens_res["Topology/Resource_allocation"] = sens_res["auction"]+'\n'+sens_res["topology"]
-# sens_res = sens_res.pivot_table(values='rank_corrected', index='config_param', columns='Topology/Resource_allocation')
-# sens_res.reset_index()
-# plot_radar(sens_res,sens_res.index.values,sens_res.columns, suffix='allocation')
-# print(sens_res.mean(axis=1))
 '''Correlation plot'''
-# sns.set(font_scale=1.2)
-# dpi = 300
-# corr['Topology/Resource allocation']= corr['auction_type']+'-'+corr['topology']
-# corr['config param'] = corr['config_param'].replace({' No. Layers': r'$L$',
-#                                                     ' No. Nodes': r'$N$',
-#                                                     ' Topology Parameter': r'$\Upsilon$',
-#                                                     ' Interconnection Prob': r'$P_i$',
-#                                                     ' Damage Prob': r'$P_d$',
-#                                                     ' Resource Cap': r'$R_c$'})
-# for y in corr.y.unique():
-#     corr_fig = corr[(corr['y']==y)&(corr['pearson_corr']!='nan')]
-#     corr_fig = corr_fig.pivot_table(values='pearson_corr',
-#                                     index='Topology/Resource allocation',
-#                                     columns='config param')
-#     plt.figure(figsize=[1500/dpi,1000/dpi])
-#     ax = sns.heatmap(corr_fig, annot=False, fmt="1.2f", vmin=-1, vmax=1,
-#                       cmap="RdYlGn")
-#     plt.savefig('corr_'+y+'.png', dpi=dpi, bbox_inches='tight')
+sns.set(font_scale=1.2)
+dpi = 300
+corr['config param'] = corr['config_param'].replace({' No. Nodes': r'$N$', ' Interconnection Prob': r'$P_i$',
+                                                     ' Damage Prob': r'$P_d$', ' Resource Cap ': r'$R_c$'})
+for y in corr.y.unique():
+    corr_fig = corr[(corr['y'] == y) & (corr['pearson_corr'] != 'nan')]
+    corr_fig = corr_fig.pivot_table(values='pearson_corr', index='Decision/Res. Alloc.', columns='config param')
+    plt.figure(figsize=[1500 / dpi, 1000 / dpi])
+    ax = sns.heatmap(corr_fig, annot=False, fmt="1.2f", vmin=-1, vmax=1,
+                     cmap="RdYlGn")
+    plt.savefig('corr_' + y + '.png', dpi=dpi, bbox_inches='tight')
+
 '''Line plot'''
-# # plt.figure()
-# # ax = sns.lineplot(x="Topology/Resource_allocation", y="rank",
-# #                   hue="config_param", style="config_param", lw=5, ms=12,
-# #                   markers=True, dashes=False, data=sens_perf)
-# ax.invert_yaxis()
+# plt.figure()
+# sns.lineplot(x="Decision/Res. Alloc.", y="rank", hue="config_param", style="config_param", lw=5, ms=12,
+#              markers=True, dashes=False, data=sens_perf)
+# plt.show()
+
 '''Bar plot'''
 plt.figure()
 c = 1
-for row in sens_perf.topology.unique():
+for row in sens_perf.decision.unique():
     for col in sens_perf.auction.unique():
         plt.subplot(3, 4, c)
         c += 1
-        data = sens_perf[(sens_perf['topology']==row)&(sens_perf['auction']==col)]
-        plt.bar(x=data['config_param'],height=data['delta'],yerr=data['delta_CI'], capsize=6)
-        plt.title(data["Topology/Resource_allocation"].unique())
+        data = sens_perf[(sens_perf['decision'] == row) & (sens_perf['auction'] == col)]
+        plt.bar(x=data['config_param'], height=data['delta'], yerr=data['delta_CI'], capsize=6)
+        plt.title(data["Decision/Res. Alloc."].unique())
         xmin, xmax, ymin, ymax = plt.axis()
-        plt.ylim(0.05,ymax)
+        plt.ylim(0.05, ymax)
 plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.35,
                     wspace=0.35)
+plt.show()
+
 '''Violin plot'''
 plt.figure()
 c = 1
@@ -125,9 +136,10 @@ for name, val in delta_dict_perf.items():
     sns.violinplot(x="variable", y="value", data=df)
     plt.title(name)
     xmin, xmax, ymin, ymax = plt.axis()
-    plt.ylim(0.05,ymax)
+    plt.ylim(0.05, ymax)
 plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.35,
                     wspace=0.35)
+plt.show()
 plt.figure()
 c = 1
 for name, val in anova_perf.items():
@@ -135,8 +147,9 @@ for name, val in anova_perf.items():
     plt.title(name)
     c += 1
     df = val['posthoc_matrix']
-    heatmap_args = {'linewidths': 0.1, 'linecolor': '0.5', 'clip_on': False, 
+    heatmap_args = {'linewidths': 0.1, 'linecolor': '0.5', 'clip_on': False,
                     'square': True, 'cbar_ax_bbox': [0.01, 0.35, 0.04, 0.3]}
     sp.sign_plot(df, **heatmap_args)
 plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.35,
                     wspace=0.35)
+plt.show()
--- codes/Sensitivity_SALib/synthetic_sa_perf.py
+++ codes/Sensitivity_SALib/synthetic_sa_perf.py
@@ -7,122 +7,84 @@ import scipy.stats as ss
 import statsmodels.api as sa
 import scikit_posthocs as sp
 
-'''Compute sensitivity'''
-[LAMBDA_DF, ALLOC_GAP_DF, RUN_TIME_DF] = pd.read_pickle('postprocess_dicts_all_topo.pkl')
+''' read and add configuration data'''
+config_list_folder = 'C:/Users/ht20/Documents/Files/Generated_Network_Dataset_v4.1/GeneralNetworks/'
+config_data = pd.read_csv(config_list_folder + 'List_of_Configurations.txt', header=0, sep="\t")
+config_data = config_data.assign(topology='general')
 
-sens_perf= pd.DataFrame(columns = ['config_param', 'auction', 'topology','delta', 'delta_CI'])
-sens_res= pd.DataFrame(columns = ['config_param', 'auction', 'topology','delta', 'delta_CI'])
+results_folder = 'C:/Users/ht20/Documents/Files/Game_synthetic/v4.1/postprocess/'
+dfs = pd.read_pickle(results_folder + 'postprocess_dicts_EDM20.pkl')
+comp_df = pd.merge(dfs[4], config_data, left_on=['Magnitude'], right_on=['Config Number'])
+comp_df['lambda_U'] = pd.to_numeric(comp_df['lambda_U'], errors='coerce')
+comp_df['T1'] = 0
+comp_df['T2'] = 0
+for idx, row in comp_df.iterrows():
+    comp_df.loc[idx, 'T1'] = comp_df.loc[idx, ' Net Types'][2]
+    comp_df.loc[idx, 'T2'] = comp_df.loc[idx, ' Net Types'][-3]
+
+'''Compute sensitivity'''
+sens_perf = pd.DataFrame(columns=['config_param', 'auction', 'decision', 'delta', 'delta_CI'])
 delta_dict_perf = {}
-delta_dict_res = {}
-topologies = ['Random', 'ScaleFree', 'Grid']
-auction_types = ['UNIFORM', 'MCA', 'MDA', 'MAA']
-for topo in topologies:
+decision_types = ['ng', 'bgCCUU', 'bgCNUU', 'bgNCUU', 'bgNNUU']
+auction_types = ['UNIFORM', 'OPTIMAL']  # 'MCA', 'MDA', 'MAA'
+for decision in decision_types:
     for auc in auction_types:
-        print(topo,auc)
-        gamma_dict={'ScaleFree':[2, 3], 'Random':[0.02, 0.62], 'Grid':[3, 10]}
+        print('Resilience', decision, auc)
         problem = {
-            'num_vars': 6,
-            'names': ['L', 'N', 'gamma', 'pi', 'pd', 'Rc'],
-            'bounds': [[2, 4], [10, 50], gamma_dict[topo], [0.001, 0.1], [0.05, 0.5], [2, 400]]}
-        print('Resilience')
-        LAMBDA_DF['lambda_U'] = LAMBDA_DF['lambda_U'].astype(float)
-        sel_LAMBDA_DF = LAMBDA_DF[(LAMBDA_DF['topology']==topo)&\
-                                  (LAMBDA_DF['auction_type']==auc)]
-        X = pd.concat([sel_LAMBDA_DF[' No. Layers'], sel_LAMBDA_DF[' No. Nodes'],
-                        sel_LAMBDA_DF[' Topology Parameter'], sel_LAMBDA_DF[' Interconnection Prob'],
-                        sel_LAMBDA_DF[' Damage Prob'], sel_LAMBDA_DF[' Resource Cap']],
-                      axis=1, keys=['L', 'N', 'gamma', 'pi', 'pd', 'Rc']).to_numpy()
+            'num_vars': 4,
+            'names': ['N', 'pi', 'pd', 'Rc'],
+            'bounds': [[10, 50], [0.001, 0.05], [0.05, 0.5], [2, 60]]}
+        comp_df['lambda_U'] = comp_df['lambda_U'].astype(float)
+        sel_LAMBDA_DF = comp_df[(comp_df['decision_type'] == decision) & (comp_df['auction_type'] == auc)
+                                & (comp_df['layer'] == 'nan')]
+        X = pd.concat([sel_LAMBDA_DF[' No. Nodes'], sel_LAMBDA_DF[' Interconnection Prob'],
+                       sel_LAMBDA_DF[' Damage Prob'], sel_LAMBDA_DF[' Resource Cap ']],
+                      axis=1, keys=['N', 'pi', 'pd', 'Rc']).to_numpy()
         Y = pd.concat([sel_LAMBDA_DF['lambda_U']], axis=1, keys=['lambda_U']).to_numpy().ravel()
         delta_perf = delta.analyze(problem, X, Y, num_resamples=100, conf_level=0.95,
-                                    print_to_console=True, seed=None)
+                                   print_to_console=True, seed=None)
         for idx, para in enumerate(delta_perf['names']):
-            sens_perf = sens_perf.append({'config_param':para, 'auction':auc,
-                                          'topology':topo, 'delta':delta_perf['delta'][idx],
-                                          'delta_CI':delta_perf['delta_conf'][idx]},
-                                          ignore_index=True)
-        delta_dict_perf[auc, topo] = pd.DataFrame({x:delta_perf['delta_vec'][idx]\
-                                                    for idx,x in enumerate(delta_perf['names'])})
-        print('Allocation')
-        ALLOC_GAP_DF['gap'] = ALLOC_GAP_DF['gap'].astype(float)
-        sel_ALLOC_GAP_DF = ALLOC_GAP_DF[(ALLOC_GAP_DF['topology']==topo)&\
-                                        (ALLOC_GAP_DF['auction_type']==auc)]
-        X = pd.concat([sel_ALLOC_GAP_DF[' No. Layers'], sel_ALLOC_GAP_DF[' No. Nodes'],
-                        sel_ALLOC_GAP_DF[' Topology Parameter'], sel_ALLOC_GAP_DF[' Interconnection Prob'],
-                        sel_ALLOC_GAP_DF[' Damage Prob'], sel_ALLOC_GAP_DF[' Resource Cap']],
-                      axis=1, keys=['L', 'N', 'gamma', 'pi', 'pd', 'Rc']).to_numpy()
-        Y = pd.concat([sel_ALLOC_GAP_DF['gap']], axis=1, keys=['gap']).to_numpy().ravel()
-        delta_res = delta.analyze(problem, X, Y, num_resamples=100, conf_level=0.95,
-                                  print_to_console=True, seed=None)
-        for idx, para in enumerate(delta_res['names']):
-            sens_res = sens_res.append({'config_param':para, 'auction':auc,
-                                        'topology':topo, 'delta':delta_res['delta'][idx],
-                                        'delta_CI':delta_res['delta_conf'][idx]},
-                                        ignore_index=True)
-        delta_dict_res[auc, topo] = pd.DataFrame({x:delta_res['delta_vec'][idx]\
-                                                    for idx,x in enumerate(delta_res['names'])})
+            sens_perf = sens_perf.append({'config_param': para, 'auction': auc,
+                                          'decision': decision, 'delta': delta_perf['delta'][idx],
+                                          'delta_CI': delta_perf['delta_conf'][idx]},
+                                         ignore_index=True)
+        delta_dict_perf[auc, decision] = pd.DataFrame.from_dict(
+            {x: delta_perf['delta_vec'][idx] for idx, x in enumerate(delta_perf['names'])})
+
 '''Compute correlation '''
 col = "auction_type"
-row = "topology"
-params = [' No. Layers', ' No. Nodes', ' Topology Parameter',
-          ' Interconnection Prob', ' Damage Prob', ' Resource Cap']
-corr= pd.DataFrame(columns = ['y', 'config_param', col, row, 'pearson_corr', 'p_value'])
-y = "gap"
-for r in topologies:
+row = "decision_type"
+params = [' No. Nodes', ' Interconnection Prob', ' Damage Prob', ' Resource Cap ']
+corr = pd.DataFrame(columns=['y', 'config_param', col, row, 'pearson_corr', 'p_value'])
+for r in decision_types:
     for c in auction_types:
         for x in params:
             print(c, r, x)
-            df_sel = ALLOC_GAP_DF[(ALLOC_GAP_DF[col]==c)&(ALLOC_GAP_DF[row]==r)]
-            pc, p = pearsonr(df_sel[x], df_sel[y])
-            print('res',c, r, pc, p)
-            corr = corr.append({'y':y, 'config_param':x, col:c, row:r, 'pearson_corr':pc,
-                                'p_value':p}, ignore_index=True)
+            df_sel = comp_df[(comp_df[col] == c) & (comp_df[row] == r)]
+            pc, p = pearsonr(df_sel[x], df_sel["lambda_U"])
+            print('perf', c, r, pc, p)
+            corr = corr.append({'y': "lambda_U", 'config_param': x, col: c, row: r, 'pearson_corr': pc,
+                                'p_value': p}, ignore_index=True)
 
-y = "lambda_U"
-for r in topologies:
-    for c in auction_types:
-        for x in params:
-            print(c, r, x)
-            df_sel = LAMBDA_DF[(LAMBDA_DF[col]==c)&(LAMBDA_DF[row]==r)]
-            pc, p = pearsonr(df_sel[x], df_sel[y])
-            print('perf',c, r, pc, p)
-            corr = corr.append({'y':y, 'config_param':x, col:c, row:r, 'pearson_corr':pc,
-                                'p_value':p}, ignore_index=True)
-
-''' compute ranking and save '''
-sens_res.loc[:, 'delta_CI/delta'] = sens_res['delta_CI']/sens_res['delta']
-for topo in topologies:
+''' compute ranking '''
+sens_perf.loc[:, 'delta_CI/delta'] = sens_perf['delta_CI'] / sens_perf['delta']
+for decision in decision_types:
     for auc in auction_types:
-        cond = (sens_res['topology']==topo)&(sens_res['auction']==auc)
-        sens_res.loc[cond, 'rank'] = sens_res.loc[cond, 'delta'].rank(method='average', ascending=False)
-sens_perf.loc[:, 'delta_CI/delta'] = sens_perf['delta_CI']/sens_perf['delta']
-for topo in topologies:
-    for auc in auction_types:
-        cond = (sens_perf['topology']==topo)&(sens_perf['auction']==auc)
+        cond = (sens_perf['decision'] == decision) & (sens_perf['auction'] == auc)
         sens_perf.loc[cond, 'rank'] = sens_perf.loc[cond, 'delta'].rank(method='average', ascending=False)
+
 ''' ANOVA and posthoc tests'''
 anova_perf = {}
-anova_res = {}
-for topo in topologies:
+for decision in decision_types:
     for auc in auction_types:
-        names = delta_perf['names']
-        data =[delta_dict_perf[auc, topo][x].values for x in names]
+        data = [delta_dict_perf[auc, decision][x].values for x in delta_perf['names']]
         H, p = ss.kruskal(*data)
-        df = pd.melt(delta_dict_perf[auc, topo], id_vars=[], value_vars=delta_dict_perf[auc, topo].columns)
+        df = pd.melt(delta_dict_perf[auc, decision], id_vars=[], value_vars=delta_dict_perf[auc, decision].columns)
         ph = sp.posthoc_conover(df, val_col='value', group_col='variable', p_adjust='holm')
-        anova_perf[auc, topo] = {'anova_p': p, 'posthoc_matrix': ph}
-
-        data =[delta_dict_res[auc, topo][x].values for x in names]
-        H, p = ss.kruskal(*data)
-        df = pd.melt(delta_dict_res[auc, topo], id_vars=[], value_vars=delta_dict_res[auc, topo].columns)
-        ph = sp.posthoc_conover(df, val_col='value', group_col='variable', p_adjust='holm')
-        anova_res[auc, topo] = {'anova_p': p, 'posthoc_matrix': ph}
+        anova_perf[auc, decision] = {'anova_p': p, 'posthoc_matrix': ph}
 
 ''' Manually correct ranks'''
-corrected = pd.read_csv('Results_perf.csv')
-sens_perf['rank_corrected'] = corrected['rank_corrected']
-corrected = pd.read_csv('Results_res.csv')
-sens_res['rank_corrected'] = corrected['rank_corrected']
-
-with open('postprocess_dicts_sens_synth.pkl', 'wb') as f:
-    pickle.dump([corr, sens_perf, sens_res, delta_dict_perf, delta_dict_res,
-                  anova_perf, anova_res], f)
+# corrected = pd.read_csv('Results_perf.csv')
+# sens_perf['rank_corrected'] = corrected['rank_corrected']
+# with open('postprocess_dicts_sens_synth.pkl', 'wb') as f:
+#     pickle.dump([corr, sens_perf, delta_dict_perf, anova_perf], f)
