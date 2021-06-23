75c670268006b59573e14c488c85f2ee58c25f78
htalebiyan <35233350+htalebiyan@users.noreply.github.com>
2021-06-23 12:42:32 -0500

    Preparing to integrate to main branch

--- pyindp/Metaheuristics/constraint_custom.m
+++
@@ -1,4 +0,0 @@
-function [c, ceq] = constraint_custom(x,constr_index,A)
-b = cell2mat(struct2cell(constr_index));
-c = A*x-b;
-ceq = [];
--- pyindp/Metaheuristics/obj_function.m
+++
@@ -1,4 +0,0 @@
-function y = obj_function(x, obj_coeffs, Ai, bi, Ae, be, lambda)
-coeffs = cell2mat(obj_coeffs);
-y = coeffs*x'+ sum(abs(Ae*x'-be))*lambda;
-% y = y + sum(Ai*x'-bi);%*lambda;
